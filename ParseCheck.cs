bb3cea498fe1e598ff3e3d4c7e7bb771e650a511
Vishal Jaishankar <t-vijais@microsoft.com>
2019-05-22 15:40:41 +0530

    blob all in one class

---
+++ ParseCheck.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace blobby
+{
+    //This class cheks if the environment variable is set
+    public class ParseCheck
+    {
+        CloudStorageAccount storageAccount = null;
+        CloudBlobContainer cloudBlobContainer = null;
+        string sourceFile = null;
+        string destinationFile = null;
+        string storageConnectionString = Environment.GetEnvironmentVariable("storageconnectionstring");
+
+        public async Task CheckParse()
+        {
+            if(CloudStorageAccount.TryParse(storageConnectionString,out storageAccount))
+            {
+                try
+                {
+                    CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+                    cloudBlobContainer = cloudBlobClient.GetContainerReference("quickstartblobs" + Guid.NewGuid().ToString());
+                    await cloudBlobContainer.CreateAsync();
+                    Console.WriteLine("Created container '{0}'", cloudBlobContainer.Name);
+                    Console.WriteLine();
+                    BlobContainerPermissions permissions = new BlobContainerPermissions
+                    {
+                        PublicAccess = BlobContainerPublicAccessType.Blob
+                    };
+                    await cloudBlobContainer.SetPermissionsAsync(permissions);
+
+                   
+                    string localPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    string localFileName = "QuickStart_" + Guid.NewGuid().ToString() + ".txt";
+                    sourceFile = Path.Combine(localPath, localFileName);
+                    // Write text to the file.
+                    File.WriteAllText(sourceFile, "Hello, World!");
+
+                    Console.WriteLine("Temp file = {0}", sourceFile);
+                    Console.WriteLine("Uploading to Blob storage as blob '{0}'", localFileName);
+                    Console.WriteLine();
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    // Use the value of localFileName for the blob name.
+                    CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(localFileName);
+                    await cloudBlockBlob.UploadFromFileAsync(sourceFile);
+
+                    // List the blobs in the container.
+                    Console.WriteLine("Listing blobs in container.");
+                    BlobContinuationToken blobContinuationToken = null;
+                    do
+                    {
+                        var resultSegment = await cloudBlobContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                        // Get the value of the continuation token returned by the listing call.
+                        blobContinuationToken = resultSegment.ContinuationToken;
+                        foreach (IListBlobItem item in resultSegment.Results)
+                        {
+                            Console.WriteLine(item.Uri);
+                        }
+                    } while (blobContinuationToken != null); 
+                    Console.WriteLine();
+
+                    
+                    destinationFile = sourceFile.Replace(".txt", "_DOWNLOADED.txt");
+                    Console.WriteLine("Downloading blob to {0}", destinationFile);
+                    Console.WriteLine();
+                    await cloudBlockBlob.DownloadToFileAsync(destinationFile, FileMode.Create);
+
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error returned from the service: {0}", ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Press any key to delete the sample files and example container.");
+                    Console.ReadLine();
+                    Console.WriteLine("Deleting the container and any blobs it contains");
+                    if (cloudBlobContainer != null)
+                    {
+                        await cloudBlobContainer.DeleteIfExistsAsync();
+                    }
+                    Console.WriteLine("Deleting the local source file and local downloaded files");
+                    Console.WriteLine();
+                    File.Delete(sourceFile);
+                    File.Delete(destinationFile);
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine(
+                   "A connection string has not been defined in the system environment variables. " +
+                   "Add a environment variable named 'storageconnectionstring' with your storage " +
+                   "connection string as a value.");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace blobby
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var first = new ParseCheck();
+            first.CheckParse().GetAwaiter().GetResult();
+               
+            //if successful
+            
+        }
+    }
+}
